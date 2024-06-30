5383d51730667f9aa73f680c38dba7f31cb4ea43
JUDAS89 <lorena.peroza@outlook.com>
2024-06-30 07:33:02 -0400

    correcciones, middleware, catch

--- server.js
+++ server.js
@@ -10,30 +10,49 @@ const loggerExpress = require('logger-express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middelware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(loggerExpress.logger());
+
+// Middelware para registrar las rutas consultadas
+app.use((req, res, next) => {
+  console.log(`Consulta a la ruta: ${req.method} ${req.url}`);
+  next();
+});
+
 // 1. Ruta GET /joyas
+// a. Devuelve la estructura HATEOAS de todas las joyas almacenadas en la base de datos
+// b. Recibe en la query string los parametros: limits, page, order by
 app.get('/joyas', async (req, res) => {
     try {
       const { limits, page, order_by } = req.query;
       let query = 'SELECT * FROM inventario';
       let params = [];
   
+      // Ordenamiento
       if (order_by) {
         const [field, direction] = order_by.split('_');
         query += ` ORDER BY ${field} ${direction}`;
       }
   
+      // Limite de recursos
       if (limits) {
         query += ' LIMIT $1';
         params.push(parseInt(limits));
       }
   
+      // Paginacion
       if (page) {
         const offset = (parseInt(page) - 1) * parseInt(limits);
         query += ' OFFSET $2';
         params.push(offset);
       }
   
+      // Consulta a la base de datos con parametros para evitar SQL Injection
       const result = await pool.query(query, params);
+      
       res.json({
         totalJoyas: result.rows.length,
         stockTotal: result.rows.reduce((acc, item) => acc + item.stock, 0),
@@ -41,8 +60,15 @@ app.get('/joyas', async (req, res) => {
           name: joya.nombre,
           href: `/joyas/${joya.id}`,
         })),
+        // Estructura de datos HATEOAS
+        _links: {
+          self: { href: `/joyas?limits=${limits}&page=${page}&order_by=${order_by}`},
+          next: { href: `/joyas?limits=${limits}&page=${parseInt(page) + 1}&order_by=${order_by}`},
+          previous: { href: `/joyas?limits=${limits}&page=${parseInt(page) -1}&order_by=${order_by}`}
+        }
       });
     } catch (error) {
+      // Usar try catch para capturar los posibles errores
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
@@ -50,12 +76,14 @@ app.get('/joyas', async (req, res) => {
 
 
 // 2. Ruta GET /joyas/filtros
+// Recibe los siguientes parametros en la query string: precio_min, precio_max, categoria, metal
 app.get('/joyas/filtros', async (req, res) => {
     try {
       const { precio_min, precio_max, categoria, metal } = req.query;
       let query = 'SELECT * FROM inventario WHERE 1=1';
       let params = [];
   
+      // Filtro de recursos por campos
       if (precio_min) {
         query += ' AND precio >= $1';
         params.push(parseInt(precio_min));
@@ -76,14 +104,17 @@ app.get('/joyas/filtros', async (req, res) => {
         params.push(metal);
       }
   
+      // Consult a la base de datos con parametros para evitar SQL Injection
       const result = await pool.query(query, params);
       res.json(result.rows);
     } catch (error) {
+      // Usar try catch para capturar los posibles errores
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
 
+  // Escuchar el servidor en el puerto configurado
   app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
   });
